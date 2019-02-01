9c78b87a080e2ac218e206c550c94338f5fb6ebf
BT_1N3_15 <wchen1325@bths.edu>
2019-02-01 14:42:29 -0500

    ok

--- src/SortingMethods.java
+++ src/SortingMethods.java
@@ -1,5 +1,6 @@
 public class SortingMethods {
 
+
     public static void bubbleSort(int[] arr){
         SortingUtil.printArray(arr);
 
@@ -109,5 +110,54 @@ public class SortingMethods {
         }
     }
 
+    public static void mergeSort(int[] arr){
+        int n = arr.length;
+        int[] temp = new int[n];
+        mergeSortHelper(arr, n , n-1, temp);
+    }
+
+    public static void mergeSortHelper(int[] arr, int left, int right, int[] temp){
+        if (left < right){
+            int mid = (left + right)/2;
+            mergeSortHelper(arr,left,mid,temp);
+            mergeSortHelper(arr,mid + 1,right,temp);
+            merge(arr,left,mid,right,temp);
+        }
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right, int[] temp) {
+        int i = left;
+        int j = mid + 1;
+        int k = right;
+
+        while ( i <= mid && j <= right){
+            if(arr[i] < arr[j]){
+                temp[k] = arr[i];
+                i++;
+            }
+            else{
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i<= mid){
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while(j <= right){
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for(k = left; k <= right; k++){
+            arr[k] = temp[k];
+        }
+
+    }
 
 }
